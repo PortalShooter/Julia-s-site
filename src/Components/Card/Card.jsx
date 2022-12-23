5902ce295642229f2536309425d34a4168ac3824
ilyakucevalov <ilya.kucevalov@gmail.com>
2022-12-23 18:52:17 +0300

    add about me section

--- src/Components/Card/Card.jsx
+++
@@ -1,9 +0,0 @@
-import React, { useEffect, useRef, useState } from 'react';
-
-const Card = () => {
-  return (
-    <div>444</div>
-  )
-}
-
-export default Card
--- src/Components/CardTS/Card.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import styles from './card.css';
-
-export function Card() {
-  return (
-    <div>333</div>
-  );
-}
